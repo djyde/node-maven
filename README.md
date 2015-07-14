1d30ffcef2ed85063529971c082c3588e6c7e731
Randy <randypriv@gmail.com>
2015-07-14 14:28:23 +0800

    add no result console

---
+++ README.md
@@ -0,0 +1,33 @@
+# node-maven
+
+node-maven is a CLI tool for helping you escape from search-copy-paste, when you wanna compile a maven central library.
+
+Based on [Maven Central API](http://search.maven.org/#api)
+
+# Quick Look
+
+![](http://ww2.sinaimg.cn/large/62580dd9gw1eu29wky8q9g20nh0e40xd.gif)
+
+# Usage
+
+**First of all, you are required to have a Node.js environment.**
+
+### Install
+
+```shell
+$ npm install node-maven -g
+```
+
+### Command
+```shell
+$ maven search <keyword>
+```
+
+Then you will get a list of search results.
+
+`Enter` to copy the maven library url to clipboard.
+
+`Backspace` to get out of list.
+
+# License
+MIT License
--- libs/api.js
+++ libs/api.js
@@ -17,28 +17,31 @@ module.exports = {
         wt: 'json'
       })
       .end(function(err,res){
-        console.log(colors.green('Founded',res.body.response.numFound,'results'));
         if(!err){
-          var list = new List({ marker: '\033[36m› \033[0m', markerLength: 2 });
-          for (var i = 0; i < res.body.response.docs.length; i++) {
-            var results = res.body.response.docs[i];
-            list.add(results.id + ':' + results.latestVersion, results.id + ' ' + results.latestVersion.yellow + ' ' + moment(results.timestamp).format('YYYY-MM-DD').green);
-            console.log(results.id)
-          };
-          list.start();
-          list.on('keypress',function(key,item){
-            switch(key.name){
-              case 'return':
-                cp.copy(item,function(){
+          if (res.body.response.docs.length === 0) {
+            console.log(colors.red('No result for',keyword));
+          } else {
+            var list = new List({ marker: '\033[36m› \033[0m', markerLength: 2 });
+            for (var i = 0; i < res.body.response.docs.length; i++) {
+              var results = res.body.response.docs[i];
+              list.add(results.id + ':' + results.latestVersion, results.id + ' ' + results.latestVersion.yellow + ' ' + moment(results.timestamp).format('YYYY-MM-DD').green);
+              console.log(results.id)
+            };
+            list.start();
+            list.on('keypress',function(key,item){
+              switch(key.name){
+                case 'return':
+                  cp.copy(item,function(){
+                    list.stop();
+                    console.log(colors.green('Copied',item));
+                  })
+                  break;
+                case 'backspace':
                   list.stop();
-                  console.log(colors.green('Copied',item));
-                })
-                break;
-              case 'backspace':
-                list.stop();
-                break;
-            }
-          })
+                  break;
+              }
+            })
+          }
         } else {
           console.log(err)
         }
